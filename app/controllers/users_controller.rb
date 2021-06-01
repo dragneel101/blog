23f3a56f16183c32b3a587519c947d06d157464e
dragneel101 <71054834+dragneel101@users.noreply.github.com>
2021-06-01 19:14:11 -0400

    add users profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,10 @@ class UsersController < ApplicationController
         
     end
 
+    def show
+        @user= User.find(params[:id])
+    end
+
     def update
         @user= User.find(params[:id])
         if @user.update(user_params)
