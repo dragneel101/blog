10c35b504035385bdbcd99117e70a9a62d0808d4
dragneel101 <71054834+dragneel101@users.noreply.github.com>
2021-06-01 18:33:50 -0400

    added update page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,22 @@ class UsersController < ApplicationController
 
     end
 
+    def edit
+        @user= User.find(params[:id])
+        
+    end
+
+    def update
+        @user= User.find(params[:id])
+        if @user.update(user_params)
+            flash[:notice] = "your profile has been updated"
+            redirect_to articles_path
+        else
+            render 'edit'
+        end
+    end
+
+    
     def create 
         @user = User.new(user_params)
         if @user.save
