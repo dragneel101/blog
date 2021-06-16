02cb3cfd5cf356d28b7982feae33b74a05fba982
dragneel101 <71054834+dragneel101@users.noreply.github.com>
2021-06-16 14:56:25 -0400

    added permission control for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
-    before_action :set_user, only: [:show, :update, :edit]
+    before_action :set_user, only: [:show, :update, :edit, :destroy]
+    before_action :require_user, only: [:edit, :update, :destroy]
+    before_action :require_same_user,only: [:edit,:update, :destroy]
+
+
 
     def new
         @user = User.new
@@ -37,6 +41,14 @@ class UsersController < ApplicationController
         end
     end
 
+    def destroy
+        @user.destroy
+        session[:user_id] = nil
+        flash[:notice] = "Account and all associated articles has been successfully deleted"
+        redirect_to articles_path
+
+    end
+
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
@@ -46,6 +58,13 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
 
     end
-        
+
+    def require_same_user
+        if current_user != @user
+            flash[:alert] = "you do not have authorization"
+            redirect_to @user
+        end
+    end
+           
     
 end
